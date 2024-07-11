f5f22a1bcb192d87a3a869c877a8ba56ef449893
Mitul Gupta <mitulgupta72>
2024-07-11 16:57:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Card1 from "./components/Card1"
+
+function App() {
+  console.log("hii")
+
+  return (
+    <>
+      <Card1></Card1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card1.jsx
@@ -0,0 +1,12 @@
+// ./components/Card1.jsx
+import React from 'react';
+
+function Card1() {
+  return (
+    <div>
+      <h1>Card 1 Component</h1>
+    </div>
+  );
+}
+
+export default Card1;
